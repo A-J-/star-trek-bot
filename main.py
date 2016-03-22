615d772f7a37d7a0501e788f2382feb94c4fb30a
A-J- <A-J-@users.noreply.github.com>
2016-03-22 15:57:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-import praw
+import praw,username
 
-userAgent = ("StarTreker for ftlgame 0.1")
+userAgent = ("StarTrekBot 0.1")
 r = praw.Reddit(user_agent = userAgent)
+r.login(username.main())
 subreddit = r.get_subreddit("ftlgame")
 
 for thread in subreddit.get_new(limit = 5):
+
 	print "Title: ",thread.title
 	print "Score: ",thread.score
 	print
 	print
+	
